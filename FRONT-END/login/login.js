5e2917e1193275c185fc093decc10284b07ae034
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-09-04 23:46:52 +0530

    made login page

---
+++ FRONT-END/login/login.js
@@ -0,0 +1,41 @@
+const loginForm = document.getElementById('loginForm');
+const email = document.getElementById('email');
+const pass = document.getElementById('pass');
+const register = document.getElementById('register');
+const forgotPassword = document.getElementById('forgotPassword');
+
+loginForm.addEventListener('submit', loginUser);
+forgotPassword.addEventListener('click', getForgotPasswordPage);
+register.addEventListener('click', signupUser);
+
+function getForgotPasswordPage(e){
+    e.preventDefault();
+    window.location.replace('../forgotPassword/forgotPassword.html');
+}
+
+function signupUser(e){
+    e.preventDefault();
+    window.location.replace('../signUp/signup.html');
+}
+
+async function loginUser(e){
+    e.preventDefault();
+    let user = {
+        email: email.value,
+        pass: pass.value
+    }
+    try{
+        const res = await axios.post('http://localhost:3000/login', user);
+        console.log(res);
+        localStorage.setItem('token', res.data.token);
+        email.value='';
+        pass.value='';
+        window.location.replace('../expense/expense.html');
+    }
+    catch(err){
+        console.log(err);
+        email.value='';
+        pass.value='';
+    }
+
+}
