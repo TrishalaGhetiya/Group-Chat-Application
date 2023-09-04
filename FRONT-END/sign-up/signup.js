24c1b2d83be7f64c25208e7a884dd4be0cae1772
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-09-04 22:16:20 +0530

    Made signup form

---
+++ FRONT-END/sign-up/signup.js
@@ -0,0 +1,38 @@
+const signUpForm = document.getElementById('signUpForm');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const phNumber = document.getElementById('phNumber');
+const pass = document.getElementById('pass');
+
+signUpForm.addEventListener('submit', createUser);
+
+async function createUser(e)
+{
+    e.preventDefault();
+    let user = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        email: email.value,
+        phNumber: phNumber.value,
+        password: pass.value
+    }
+    try{
+        const res = await axios.post('http://localhost:3000/signup', user);
+        console.log(res);
+        firstName.value='';
+        lastName.value='';
+        email.value='';
+        phNumber.value='';
+        pass.value='';
+        //window.location.replace('../login/login.html');
+    }
+    catch(err){
+        console.log(err);
+        firstName.value='';
+        lastName.value='';
+        email.value='';
+        phNumber.value='';
+        pass.value='';
+    }
+}
